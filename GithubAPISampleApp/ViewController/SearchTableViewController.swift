8b22dead5b197e26786e993349b9a542544db040
NishizawaShota <nishizawa@emol.jp>
2019-08-22 14:12:26 +0900

    メインの基本的なView部分を実装

--- GithubAPISampleApp/ViewController/SearchTableViewController.swift
+++ GithubAPISampleApp/ViewController/SearchTableViewController.swift
@@ -8,12 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SearchTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        print("test")
+        
     }
 
 
