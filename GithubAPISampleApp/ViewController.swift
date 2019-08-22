bcdefdd88d23230a81d810a2bb42857783abd077
NishizawaShota <nishizawa@emol.jp>
2019-08-22 13:51:40 +0900

    Initial Commit

---
+++ GithubAPISampleApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GithubAPISampleApp
+//
+//  Created by Shota Nishizawa on 2019/08/22.
+//  Copyright © 2019 Shota Nishizawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
