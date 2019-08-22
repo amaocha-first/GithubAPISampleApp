ebb734a3fa04510ac8226a7c18a715a8c8f75ad9
NishizawaShota <nishizawa@emol.jp>
2019-08-22 18:17:15 +0900

    ResultEntityの定義

---
+++ GithubAPISampleApp/Model/SearchResultEntity.swift
@@ -0,0 +1,18 @@
+//
+//  SearchResultEntity.swift
+//  GithubAPISampleApp
+//
+//  Created by Shota Nishizawa on 2019/08/22.
+//  Copyright © 2019 Shota Nishizawa. All rights reserved.
+//
+
+import Foundation
+
+struct SearchResultEntity {
+    
+    var repositoryName: String
+    var languageName: String
+    var userIconImage: String
+    var urlBtnText: String
+    
+}
